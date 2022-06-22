53c752c7724ae2aa241294ee219b13d11e5f9abc
marcinzygan <soul@o2.pl>
2022-06-22 20:09:30 +0100

    dependency added in UseEffect/App.js , Readme updated

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   useEffect(() => {
     dispatch(getCartItems());
-  }, []);
+  }, [dispatch]);
   useEffect(() => {
     dispatch(cartCalcTotals());
   }, [cartItems, dispatch]);
